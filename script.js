32f410c324d841a5d9174e47adbd92d03a908ba1
Twinkal Narnaware <94154988+twinkleUniverse@users.noreply.github.com>
2024-01-22 00:50:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+console.log("start");
+
+setTimeOut(function () {
+	console.log("Macrotask 1");
+},0);
+
+promise.resolve().then(function(){
+	console.log("Microtask 2");
+});
+console.log("end");
